19170c21ffc5552a7bd6755694315342e0bba7b2
jeffads92-spec <jeffads92@gmail.com>
2026-01-31 22:01:09 +0700

    Add Flask configuration management in config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = os.environ.get('DEBUG', default=False)
+
+# Add any additional configuration options here
